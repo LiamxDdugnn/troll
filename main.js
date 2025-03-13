04e3e8f4f43701164d254e08dd0b9933d0d3cf58
phạm tiến dũng <tdungx02@gmail.com>
2025-03-13 20:14:42 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const openButton = document.getElementById("openButton");
-    const flowerContainer = document.getElementById("flowerContainer");
-    const greetingMessage = document.getElementById("greetingMessage");
-
-    openButton.addEventListener("click", function () {
-        flowerContainer.classList.remove("hidden");
-        greetingMessage.classList.remove("hidden");
-        openButton.style.display = "none";
-    });
-});
+onload = () =>{
+        document.body.classList.remove("container");
+};
